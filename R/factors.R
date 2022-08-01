1130ee9f0a3e0ae4008f93e4be0ae57d7ca3582e
Gabe Neslin <gabeneslin@gmail.com>
2022-07-31 19:17:04 -0600

    Initial Commit

---
+++ R/factors.R
@@ -0,0 +1,25 @@
+#' A function to return the prime factorization of a given number as a list
+#'
+#' @param n A number (preferably an integer).
+#'
+#' @return The prime factorization of \code{n}.
+#' @export
+#'
+#' @examples
+#' prime_factors(10)
+prime_factors <- function(n) {
+  stopifnot(is.numeric(n), length(n) == 1)
+  n <- as.integer(n)
+  s <- vector()
+  k <- 2L
+
+  while (n >= 1) {
+    if(n %% k == 0){
+      n <-  n / k
+      s <- c(s, k)
+    } else{
+      k <- k + 1
+    }
+  }
+  return(s)
+}
